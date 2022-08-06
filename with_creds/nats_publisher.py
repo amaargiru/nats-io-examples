bb3c26ec81f4b6e3e31d6e17d8248f0965cc2c21
amaargiru <war4one@gmail.com>
2022-08-06 16:16:47 +0500

    Minor changes

--- with_creds/nats_publisher.py
+++ with_creds/nats_publisher.py
@@ -4,17 +4,17 @@ import nats
 
 
 async def main():
-    nc = await nats.connect("nats://localhost:4222", user_credentials="user.creds")
+    nats_connector = await nats.connect("nats://localhost:4222", user_credentials="user.creds")
     print("Run NATS publisher")
 
     count: int = 0
     while True:
         print(".", end="")
-        await nc.publish("foo", f"Hello #{count} from NATS publisher".encode('ascii'))
+        await nats_connector.publish("foo", f"Hello #{count} from NATS publisher".encode('ascii'))
         await asyncio.sleep(1)
         count += 1
 
-    await nc.close()
+    await nats_connector.close()
 
 
 if __name__ == '__main__':
--- with_creds/nats_subscriber.py
+++ with_creds/nats_subscriber.py
@@ -7,14 +7,14 @@ async def main():
     async def message_handler(msg):
         print(f"Received a message from \"{msg.subject} {msg.reply}\": {msg.data.decode()}")
 
-    nc = await nats.connect("nats://localhost:4222", user_credentials="user.creds")
-    sub = await nc.subscribe("foo", cb=message_handler)
+    nats_connector = await nats.connect("nats://localhost:4222", user_credentials="user.creds")
+    sub = await nats_connector.subscribe("foo", cb=message_handler)
     print("Run NATS subscriber")
 
     while True:
         await asyncio.sleep(1)
 
-    await nc.close()
+    await nats_connector.close()
 
 
 if __name__ == '__main__':
